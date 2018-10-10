b3a97267156922fdc7a5eb88351135e28cc455c7
bekomay26 <bekomay26@gmail.com>
2018-10-10 16:34:41 +0100

    chore(setup): redis setup (#7)
    - add predis library
    - test configuration
    
    [Finishes #160710444]

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,20 @@
 */
 
 Route::get('/', function () {
+
+    // store data into redis for the next 20 seconds
+    Redis::setex("user", 20, "testUser");
+
     return view('welcome');
+});
+
+// To test redis
+Route::get('/redis', function () {
+
+    // Increments on every page load, refresh included
+    $visits = Redis::incr('visits');
+
+    //retrieve the stored user and concatenates value to the visits value
+    $result = Redis::get("user") . ' ' . $visits;
+    return $result;
 });
